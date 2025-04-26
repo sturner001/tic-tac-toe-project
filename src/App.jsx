6734a3afad3b21055243af93ed0a96a4c044ebb2
Sam Turner <sam.turner@kokadjo.net>
2025-04-25 23:05:28 -0400

    started Player component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
 
-function App() {
-  
+import Player from './components/Player';
 
-  return (
-    <h1>React Tic-Tac-Toe</h1>
-  )
+
+function App() {
+
+
+  return <main>
+    <div id="game-container">
+      <ol id="players">
+        <Player name={'Player 1'} symbol={'X'} />
+        <Player name={'Player 2'} symbol={'O'} />
+      </ol>
+
+      GAME BOARD
+    </div>
+    LOG
+  </main>
 }
 
 export default App
